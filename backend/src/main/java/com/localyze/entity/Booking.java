73eee9ddbb2b15a7fb4148c7813859db64e5dd25
YogeshDharya <yogeshdharya@gmail.com>
2026-07-31 19:59:01 +0530

    temporary changes

--- backend/src/main/java/com/localyze/entity/Booking.java
+++ backend/src/main/java/com/localyze/entity/Booking.java
@@ -1,14 +1,24 @@
 package com.localyze.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import com.localyze.entity.enums.BookingStatus;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -22,17 +32,17 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+    private Long userId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "service_id", nullable = false)
-    private ServiceEntity service;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "service_id", nullable = false)
+    private Long serviceId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "seller_id", nullable = false)
-    private User seller;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "seller_id", nullable = false)
+    private Long sellerId;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 15)
